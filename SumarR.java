15e803c9b6f96f2fbca9fb229ef232f7242c0a88
JeanBlanco <jeanrinci.br1@gmail.com>
2025-08-10 20:05:29 -0500

    sumas de numeros primos - recursividad

---
+++ SumarR.java
@@ -0,0 +1,13 @@
+public class SumarR {
+    public static void main(String[] args) {
+        System.out.println(s(10));
+    }
+    public static int s(int n){
+        if (n == 1) {
+            return 1;
+            
+        }else{
+            return s(n-1) + n;
+        }
+    }
+}
